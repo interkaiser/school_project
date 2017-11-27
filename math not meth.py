f8af4e87bff10dafd9d04a7320dec4942ae9d010
TwoPizzas <unickyzer@gmail.com>
2017-11-27 21:05:10 +0300

    Update math not meth.py

--- math not meth.py
+++ math not meth.py
@@ -1,17 +1,25 @@
-g = 9.81
+g = -9.81
 r = 1
 mass = 1
+all = []
 
-def dist(x, y):
+def vectsum(x, y):
     return (x**2 + y**2)**(1/2)
 
 def force(this, that):
-    return g*(that - this)/(2*r)
+    z = vectsum(this.x - that.x, this.y - that.y)
+    if z > r*2:
+        return -1/z**6*(this.x - that.x)/2, -1/z**6*(this.y - that.y)/2
+    if z < r*2:
+        return 1/z**8*(this.x - that.x)/2, 1/z**8*(this.y - that.y)/2
+    else:
+        return 0, 0
 
 class Particle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        all.append(self)
 
     def move(self, x, y):
         self.x += x
@@ -20,8 +28,13 @@ class Particle:
 def getforce(p):
     vertforce = g
     horforce = 0
-    for d in Particle:
-        if(dist(p.x - d.x, p.y - d.y) <= 2*r):
-            vertforce += force(p.y, d.y)
-            horforce += force(p.x, d.x)
-    return vertforce, horforce
+    for d in all:
+        if d is not p:
+            vertforce += force(p, d)[1]
+            horforce += force(p, d)[0]
+    return horforce, vertforce
+
+
+p = Particle(0,0)
+d = Particle(1,1)
+print(getforce(d))
