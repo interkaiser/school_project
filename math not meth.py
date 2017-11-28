a67d892b14e60160ccc52da5c4b53f4372191205
TwoPizzas <unickyzer@gmail.com>
2017-11-28 21:42:59 +0300

    Update math not meth.py
    WARNING: DOES NOT WORK

--- math not meth.py
+++ math not meth.py
@@ -1,40 +1,61 @@
 g = -9.81
-r = 1
+r = 0.5
 mass = 1
 all = []
 
-def vectsum(x, y):
-    return (x**2 + y**2)**(1/2)
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def force(this, that):
-    z = vectsum(this.x - that.x, this.y - that.y)
-    if z > r*2:
-        return -1/z**6*(this.x - that.x)/2, -1/z**6*(this.y - that.y)/2
-    if z < r*2:
-        return 1/z**8*(this.x - that.x)/2, 1/z**8*(this.y - that.y)/2
-    else:
-        return 0, 0
+    def __add__(self, other):
+        self.x += other.x
+        self.y += other.y
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return Vector(self.x / other, self.y / other)
+
+    def __abs__(self):
+        return (self.x**2+self.y**2)**(1/2)
+
 
 class Particle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.v = Vector(0, 0)
         all.append(self)
 
-    def move(self, x, y):
-        self.x += x
-        self.y += y
+    def move(self):
+        if self.x + self.v.x >= 0:
+            self.x += self.v.x
+        if self.y + self.v.y >= 0:
+            self.y += self.v.y
 
-def getforce(p):
-    vertforce = g
-    horforce = 0
+    def accel(self, a):
+        self.v += a
+
+def force(self, other):
+    z = Vector(self.x - other.x, self.y - other.y)
+    z /= abs(z)
+    z *= (1/abs(z)**5 - 1/abs(z)**7)
+    return z
+
+def getforces(p):
+    f = Vector(0, g)
     for d in all:
         if d is not p:
-            vertforce += force(p, d)[1]
-            horforce += force(p, d)[0]
-    return horforce, vertforce
+            f += force(p, d)
+    return f
 
 
 p = Particle(0,0)
 d = Particle(1,1)
-print(getforce(d))
+f = Particle(1,0)
+print(d.x, d.y)
+d.accel(getforces(d))
+d.move
+print(d.x, d.y)
