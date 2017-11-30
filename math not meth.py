baf0049dbab91a7b2e60c1d667720ece2cb92fa8
TwoPizzas <unickyzer@gmail.com>
2017-11-30 22:08:57 +0300

    Update math not meth.py

--- math not meth.py
+++ math not meth.py
@@ -1,16 +1,15 @@
-g = -9.81
-r = 0.5
-mass = 1
-all = []
+mol = []
+g = -0.025
 
 class Vector:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x, self.y = x,y
 
     def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
         return Vector(self.x * other, self.y * other)
@@ -21,13 +20,10 @@ class Vector:
     def __abs__(self):
         return (self.x**2+self.y**2)**(1/2)
 
-
 class Particle:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.v = Vector(0, 0)
-        all.append(self)
+        self.x, self.y, self.v = x, y, Vector(0, 0)
+        mol.append(self)
 
     def move(self):
         if self.x + self.v.x >= 0:
@@ -35,27 +31,20 @@ class Particle:
         if self.y + self.v.y >= 0:
             self.y += self.v.y
 
-    def accel(self, a):
+    def acc(self):
+        a = Vector(0, g)
+        for i in mol:
+            if i is not self:
+                z = Vector(i.x - self.x, i.y - self.y)
+                r = a - z / abs(z) ** 8 + z / abs(z) ** 6
+                if (abs(r) < 0.9*abs(z)) | (abs(r) > 1.1*abs(z)):
+                    a = r
         self.v += a
 
-def force(self, other):
-    z = Vector(self.x - other.x, self.y - other.y)
-    z /= abs(z)
-    z *= (1/abs(z)**5 - 1/abs(z)**7)
-    return z
 
-def getforces(p):
-    f = Vector(0, g)
-    for d in all:
-        if d is not p:
-            f += force(p, d)
-    return f
-
-
-p = Particle(0,0)
-d = Particle(1,1)
-f = Particle(1,0)
-print(d.x, d.y)
-d.accel(getforces(d))
-d.move
-print(d.x, d.y)
+p = Particle(0, 0)
+d = Particle(1.2, 0)
+for i in range(100):
+    d.acc()
+    d.move()
+    print(d.x, d.y)
